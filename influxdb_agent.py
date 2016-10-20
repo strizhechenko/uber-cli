30f4930e80611c8236e16869a4e16dd6ce78afbb
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-20 11:06:33 +0500

    Refactoring(influxdb_agent): less hardcode, only list of points names left
    You only need to define PLACES and BLACKLISTED_ROUTES (please, respect uber,
    don't create useless workload to their API servers)
    
    PLACES = ('home', 'work', 'vost', 'engl')
    BLACKLISTED_ROUTES = ('home2engl', 'vost2engl', 'engl2work')
    
    code will lookup HOME_LAT, HOME_LNG, ... env variables

--- influxdb_agent.py
+++ influxdb_agent.py
@@ -3,29 +3,27 @@
 """ Is Surge Gone? """
 
 from os import getenv
+from itertools import product
 from requests import post
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
 
 INFLUXDB_URL = getenv('INFLUXDB_URL', 'http://127.0.0.1:8086/write?db=uber')
-HOME_LAT = getenv('HOME_LAT')
-HOME_LNG = getenv('HOME_LNG')
-WORK_LAT = getenv('WORK_LAT')
-WORK_LNG = getenv('WORK_LNG')
-VOST_LAT = getenv('VOST_LAT')
-VOST_LNG = getenv('VOST_LNG')
 SERVER_TOKEN = getenv('SERVER_TOKEN')
-HOME_POINT = (HOME_LAT, HOME_LNG)
-WORK_POINT = (WORK_LAT, WORK_LNG)
-VOST_POINT = (VOST_LAT, VOST_LNG)
+PLACES = ('home', 'work', 'vost', 'engl')
+BLACKLISTED_ROUTES = ('home2engl', 'vost2engl', 'engl2work')
+
+
+def get_point(name):
+    return (getenv("{0}_LAT".format(name.upper())), getenv("{0}_LNG".format(name.upper())))
 
 
 def uber_start_price_estimate(client, start_point, end_point):
     prices = uber_price_estimate(client, start_point, end_point)
     price = None
-    for product in prices:
-        if product['localized_display_name'] == u'uberSTART':
-            price = product
+    for uber_type in prices:
+        if uber_type['localized_display_name'] == u'uberSTART':
+            price = uber_type
     if not price:
         raise ValueError("No response from UBER")
     return price
@@ -48,23 +46,14 @@ def send2influxdb(price, path):
 def main():
     """ Main scenario """
     client = UberRidesClient(Session(server_token=SERVER_TOKEN))
-
-    price = uber_start_price_estimate(client, HOME_POINT, WORK_POINT)
-    send2influxdb(price, 'home2work')
-    price = uber_start_price_estimate(client, WORK_POINT, HOME_POINT)
-    send2influxdb(price, 'work2home')
-
-    price = uber_start_price_estimate(client, WORK_POINT, VOST_POINT)
-    send2influxdb(price, 'work2vost')
-    price = uber_start_price_estimate(client, VOST_POINT, WORK_POINT)
-    send2influxdb(price, 'vost2work')
-
-    price = uber_start_price_estimate(client, HOME_POINT, VOST_POINT)
-    send2influxdb(price, 'home2vost')
-    price = uber_start_price_estimate(client, VOST_POINT, HOME_POINT)
-    send2influxdb(price, 'vost2home')
+    points = {k: get_point(k) for k in PLACES}
+    for route in product(PLACES, PLACES):
+        route_name = "{0}2{1}".format(route[0], route[1])
+        if route[0] == route[1] or route_name in BLACKLISTED_ROUTES:
+            continue
+        price = uber_start_price_estimate(client, points[route[0]], points[route[1]])
+        send2influxdb(price, route_name)
 
 
 if __name__ == '__main__':
     main()
-
